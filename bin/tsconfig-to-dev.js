f059a3d5c8bb202173733f4921fdf1131aee637f
Octopodo <orson_wells40@hotmail.com>
2025-03-19 18:27:31 +0100

    Added suport for tsconfig.tests.ts

--- bin/tsconfig-to-dev.js
+++ bin/tsconfig-to-dev.js
@@ -2,16 +2,20 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function restoreTsconfig() {
-  const tsconfigPath = path.join(process.cwd(), 'tsconfig.json');
-  const backupPath = path.join(process.cwd(), 'tsconfig.json.backup');
+async function restoreTsconfig(
+  configs = ['tsconfig.json', 'tsconfig.tests.json']
+) {
+  for (const config of configs) {
+    const tsconfigPath = path.join(process.cwd(), config);
+    const backupPath = path.join(process.cwd(), `${config}.backup`);
 
-  try {
-    await fs.copyFile(backupPath, tsconfigPath);
-    console.log('tsconfig.json restored');
-    fs.rm(backupPath);
-  } catch (error) {
-    console.log(`Error deleting backup file: ${error}`);
+    try {
+      await fs.copyFile(backupPath, tsconfigPath);
+      console.log(`${config} restored`);
+      fs.rm(backupPath);
+    } catch (error) {
+      console.log(`Error deleting backup file: ${error}`);
+    }
   }
 }
 
--- bin/tsconfig-to-prod.js
+++ bin/tsconfig-to-prod.js
@@ -2,29 +2,40 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function adjustTsconfig() {
-  const tsconfigPath = path.join(process.cwd(), 'tsconfig.json');
-  const tsconfig = JSON.parse(await fs.readFile(tsconfigPath, 'utf-8'));
-  const tsconfigBackupPath = path.join(process.cwd(), 'tsconfig.json.backup');
-
-  await fs.copyFile(tsconfigPath, tsconfigBackupPath);
-  // Modifica las rutas en "types"
-  tsconfig.compilerOptions.types = tsconfig.compilerOptions.types.map(
-    (type) => {
-      if (type.startsWith('./node_modules/types-for-adobe')) {
-        return type.replace(
-          './node_modules/types-for-adobe',
-          '../types-for-adobe'
-        );
-      }
-      return type;
+async function adjustTsconfig(
+  configs = ['tsconfig.json', 'tsconfig.tests.json']
+) {
+  for (const config of configs) {
+    const tsconfigPath = path.join(process.cwd(), config);
+    const tsconfig = JSON.parse(await fs.readFile(tsconfigPath, 'utf-8'));
+    const tsconfigBackupPath = path.join(process.cwd(), `${config}.backup`);
+    try {
+      await fs.copyFile(tsconfigPath, tsconfigBackupPath);
+    } catch (error) {
+      console.log(`Error creating backup file: ${error}`);
+      return;
     }
-  );
+    // Modifica las rutas en "types"
+    tsconfig.compilerOptions.types = tsconfig.compilerOptions.types.map(
+      (type) => {
+        if (type.startsWith('./node_modules/types-for-adobe')) {
+          return type.replace(
+            './node_modules/types-for-adobe',
+            '../types-for-adobe'
+          );
+        }
+        return type;
+      }
+    );
 
-  // Escribe el tsconfig modificado
-
-  await fs.writeFile(tsconfigPath, JSON.stringify(tsconfig, null, 2), 'utf-8');
-  console.log('tsconfig.json modified for production');
+    // Escribe el tsconfig modificado
+    await fs.writeFile(
+      tsconfigPath,
+      JSON.stringify(tsconfig, null, 2),
+      'utf-8'
+    );
+    console.log(`${config} modified for production`);
+  }
 }
 
 adjustTsconfig().catch(console.error);
